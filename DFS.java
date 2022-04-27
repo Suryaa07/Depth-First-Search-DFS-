30f830e8f9f4ebf29ebc7c4d3496f4576ab2c8d7
Suryaa07 <72430187+Suryaa07@users.noreply.github.com>
2022-04-27 17:28:14 +0530

    Update DFS.java

--- DFS.java
+++ DFS.java
@@ -41,11 +41,11 @@ class Graph {
 		Graph g = new Graph(4);
 
 		g.addEdge(0, 1);
-		g.addEdge(0, 2);
+		g.addEdge(1, 2);
 		g.addEdge(1, 2);
 		g.addEdge(2, 0);
-		g.addEdge(2, 3);
-		g.addEdge(3, 3);
+		g.addEdge(2, 2);
+		g.addEdge(3, 0);
 
 		System.out.println("Following is Depth First Traversal "+ "(starting from vertex 2)");
 
